7efcba05532970355be7524ca9b021bcc6f0d672
AmarBarde <amresh.spitr2019@gmail.com>
2023-12-26 15:40:06 +0530

    this is user interface for school

---
+++ Ui.js
@@ -0,0 +1,60 @@
+let form = document.querySelector("form")
+
+let ui = [];
+
+form.addEventListener("submit", data)
+    function data(event){
+        event.preventDefault()
+
+        let name = document.querySelector("#name").value;
+
+        let ID = document.querySelector("#id").value;
+        
+        let subject = document.querySelector("#subject").value;
+        
+        let qualification = document.querySelector("#qualification").value
+        
+        let btn = document.querySelector("#btn").value
+
+        let object = {
+            DName : name,
+            Did : ID,
+            Dsubject : subject,
+            Dquali : qualification,
+            Dbtn : btn
+        }
+
+        ui.push(object)
+        console.log(ui);
+        Display(ui)
+    }
+      function  Display(ui){
+        document.querySelector("tbody").innerHTML = "";
+        ui.forEach(function (object) {
+
+            let tr = document.createElement("tr")
+
+            let name = document.createElement("td")
+            name.innerText = object.DName
+
+            let id = document.createElement("td")
+            id.innerText = object.Did
+
+            let subject = document.createElement("td")
+            subject.innerText = object.Dsubject
+
+            let quali = document.createElement("td")
+            quali.innerText = object.Dquali
+
+            let Delete = document.createElement("td")
+            Delete.innerText = "delete"
+
+            Delete.addEventListener("click", function(event){
+                event.target.parentNode.remove()
+            })
+
+            tr.append(name, id, subject, quali, Delete,);
+            document.querySelector("tbody").append(tr); 
+        });
+      }
+
